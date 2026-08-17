7f2bba9f6b53b46c1cba4a59312baf8a57b1ab0b
eramansingh40-bit <eramansingh40@gmail.com>
2026-08-18 00:14:47 +0530

    Update incidents.py

--- app/incidents.py
+++ app/incidents.py
@@ -1,1 +1,24 @@
+import datetime
 
+
+incidents = []
+
+
+def create_incident(alert):
+    incident = {
+        "id": f"INC-{len(incidents) + 1:03d}",
+        "timestamp": datetime.datetime.now().isoformat(),
+        "indicator": alert["indicator"],
+        "severity": alert["severity"],
+        "event_type": alert["event_type"],
+        "description": alert["description"],
+        "status": "Open"
+    }
+
+    incidents.append(incident)
+
+    return incident
+
+
+def get_incidents():
+    return incidents
