79620c17b207d6208b6e7f4e22a0b31f2bb3650b
eramansingh40-bit <eramansingh40@gmail.com>
2026-08-18 00:13:41 +0530

    Update threat_intel.py

--- app/threat_intel.py
+++ app/threat_intel.py
@@ -1,1 +1,17 @@
+import json
 
+
+def load_indicators():
+    with open("data/indicators.json", "r") as file:
+        return json.load(file)
+
+
+def search_indicator(indicator):
+    data = load_indicators()
+
+    for category in data.values():
+        for item in category:
+            if item["indicator"].lower() == indicator.lower():
+                return item
+
+    return None
